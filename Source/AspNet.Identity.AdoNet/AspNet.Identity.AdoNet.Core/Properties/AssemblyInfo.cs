c4d3593e59a4a8ebb1acc75bd9d602a0ab1d12c3
Alexander <alexander-87@users.noreply.github.com>
2016-03-19 23:14:41 -0500

    Updating AssemblyInfo

--- Source/AspNet.Identity.AdoNet/AspNet.Identity.AdoNet.Core/Properties/AssemblyInfo.cs
+++ Source/AspNet.Identity.AdoNet/AspNet.Identity.AdoNet.Core/Properties/AssemblyInfo.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AspNet.Identity.AdoNet")]
-[assembly: AssemblyDescription("Custom Storage Provider for ASP.NET Identity using ADO.NET")]
+[assembly: AssemblyDescription("Core library for custom ASP.NET Identity storage providers using ADO.NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AspNet.Identity.AdoNet")]
@@ -34,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyInformationalVersion("0.9.0-alpha")]
