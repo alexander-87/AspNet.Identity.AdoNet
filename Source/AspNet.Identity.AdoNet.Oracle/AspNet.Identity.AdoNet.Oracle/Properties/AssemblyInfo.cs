624f121d03f856bf064aaf9247002e8ec75a8d50
Alexander <alexander-87@users.noreply.github.com>
2016-03-20 00:01:41 -0500

    Creating new solution and project for Oracle storage provider.

---
+++ Source/AspNet.Identity.AdoNet.Oracle/AspNet.Identity.AdoNet.Oracle/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AspNet.Identity.AdoNet.Oracle")]
+[assembly: AssemblyDescription("Core library for custom ASP.NET Identity storage providers using ADO.NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AspNet.Identity.AdoNet")]
+[assembly: AssemblyCopyright("Copyright © Alexander 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the TypeLib if this project is exposed to COM
+[assembly: Guid("728ff8c1-bf2f-4302-80dc-871c71cfe9f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyInformationalVersion("0.9.0-alpha")]
