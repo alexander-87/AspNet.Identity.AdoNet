3604c041a25f2dbabcd2786ee6f223e7b3266619
Alexander <alexander-87@users.noreply.github.com>
2016-03-23 17:47:07 -0500

    Explicitly adding argument names & fixing incorrect mapping of Users table's column names

--- Source/AspNet.Identity.AdoNet.Oracle/AspNet.Identity.AdoNet.Oracle/Properties/AssemblyInfo.cs
+++ Source/AspNet.Identity.AdoNet.Oracle/AspNet.Identity.AdoNet.Oracle/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.9.0.0")]
 [assembly: AssemblyFileVersion("0.9.0.0")]
-[assembly: AssemblyInformationalVersion("0.9.3-alpha")]
+[assembly: AssemblyInformationalVersion("0.9.4-alpha")]
